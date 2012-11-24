929b1baf5a667ac2216596e1f845185c9c885aec
Alexander Klee <aklee@catfp.ch>
2012-11-24 12:02:55 +0100

    Added ProductDescription and implemented the writeProduct function

---
+++ TweakToolkitSolution/TweakToolkit.WCF.Test/ServiceDomain/ProductDescription.cs
@@ -0,0 +1,195 @@
+﻿namespace TweakToolkit.WCF.Test.ServiceDomain
+{
+    public class ProductDescription
+    {
+        public string AssetClass { get; set; }
+
+        public string AssetClassEn { get; set; }
+
+        public string bonuslevel { get; set; }
+
+        public string bonuslevelEn { get; set; }
+
+        public string CallData { get; set; }
+
+        public string CallDataEn { get; set; }
+
+        public string Cap { get; set; }
+
+        public string capEn { get; set; }
+
+        public string ConditionalObservation { get; set; }
+
+        public string couponBedingtEn { get; set; }
+
+        public string CouponGuaranteed_En { get; set; }
+
+        public string CouponObservation { get; set; }
+
+        public string cpbedingt { get; set; }
+
+        public string cpbedingtEn { get; set; }
+
+        public string CPFloater { get; set; }
+
+        public string CPFloaterEn { get; set; }
+
+        public string CpGuaranteed { get; set; }
+
+        public string CpGuaranteedEn { get; set; }
+
+        public string Currency { get; set; }
+
+        public string CurrencyRisk { get; set; }
+
+        public string CurrencyRiskEn { get; set; }
+
+        public string Dirty { get; set; }
+
+        public string Discount { get; set; }
+
+        public string DiscountEn { get; set; }
+
+        public string EarlyRedemption { get; set; }
+
+        public string EarlyRedemptionEn { get; set; }
+
+        public string EmissionPrice { get; set; }
+
+        public string EmissionType { get; set; }
+
+        public System.DateTime EndFixationDate { get; set; }
+
+        public string Floater { get; set; }
+
+        public string FloaterEn { get; set; }
+
+        public string Guarantor { get; set; }
+
+        public bool HasEnglish { get; set; }
+
+        public string ISIN { get; set; }
+
+        public string Issuer { get; set; }
+
+        public string issuerNameShort { get; set; }
+
+        public string LeadManager { get; set; }
+
+        public string maxCoupon { get; set; }
+
+        public string maxCouponEn { get; set; }
+
+        public string MaxPayback { get; set; }
+
+        public string MaxPaybackEn { get; set; }
+
+        public string MaxYield { get; set; }
+
+        public string MaxYieldEn { get; set; }
+
+        public string minCoupon { get; set; }
+
+        public string minCouponEn { get; set; }
+
+        public string MinPayback { get; set; }
+
+        public string MinPaybackEn { get; set; }
+
+        public string Name { get; set; }
+
+        public string NameEn { get; set; }
+
+        public string Nominal { get; set; }
+
+        public string Partizipation { get; set; }
+
+        public string PartizipationEn { get; set; }
+
+        public string PartizipationText { get; set; }
+
+        public string PartizipationTextEn { get; set; }
+
+        public string Pricing { get; set; }
+
+        public string PricingEn { get; set; }
+
+        public string ProductCat { get; set; }
+
+        public string ProductCatEn { get; set; }
+
+        public int productKatId { get; set; }
+
+        public string ProductState { get; set; }
+
+        public string ProductType { get; set; }
+
+        public string ProductTypeEn { get; set; }
+
+        public int productTypeId { get; set; }
+
+        public string Protection { get; set; }
+
+        public string ProtectionEn { get; set; }
+
+        public string ProtectionType { get; set; }
+
+        public System.DateTime RedemptionDate { get; set; }
+
+        public string s1t { get; set; }
+
+        public string s1tEN { get; set; }
+
+        public string s1v { get; set; }
+
+        public string s1vEn { get; set; }
+
+        public string s2t { get; set; }
+
+        public string s2tEn { get; set; }
+
+        public string s2v { get; set; }
+
+        public string s2vEn { get; set; }
+
+        public string s3t { get; set; }
+
+        public string s3tEn { get; set; }
+
+        public string s3v { get; set; }
+
+        public string s3vEn { get; set; }
+
+        public string SmallestTradeableUnit { get; set; }
+
+        public System.DateTime StartFixationDate { get; set; }
+
+        public string StockExchange { get; set; }
+
+        public string Symbol { get; set; }
+
+        public string Text_Bed_Coupon { get; set; }
+
+        public string textBedCoupon_En { get; set; }
+
+        public string TextCoupon { get; set; }
+
+        public string TextCouponEn { get; set; }
+
+        public string TextEarlyRedemtpion { get; set; }
+
+        public string TextEarlyRedemtpionEn { get; set; }
+
+        public string TextFloater { get; set; }
+
+        public string TextFloaterEn { get; set; }
+
+        public string TextMaxPayback { get; set; }
+
+        public string TextMaxPaybackEn { get; set; }
+
+        public int Valor { get; set; }
+
+        public System.DateTime ValutaDate { get; set; }
+    }
+}
