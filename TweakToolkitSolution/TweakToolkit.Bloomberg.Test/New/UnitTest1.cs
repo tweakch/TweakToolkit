1ae845d2d5ea2a21ad280b87143bf9f1b08d88a1
unknown <akl@vw8s17.ads.cat-holding.ch>
2012-11-27 00:20:09 +0100

    included packages information so a newly added repository can be built

---
+++ TweakToolkitSolution/TweakToolkit.Bloomberg.Test/New/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TweakToolkit.Bloomberg.New;
+
+namespace TweakToolkit.Bloomberg.Test.New
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestTokenCreation()
+        {
+            SubscriptionSession session = new Bloomberg.New.SubscriptionSession();
+            var token = session.GenerateToken();
+            Assert.IsNotNull(token);
+        }
+    }
+}
